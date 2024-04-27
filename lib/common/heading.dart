c584126d0dd368eac306da6f15b018ddc63d4864
geraldkachi <fitzgeraldkachi@gmail.com>
2024-04-27 20:22:39 +0100

    category page

---
+++ lib/common/heading.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_vector_icons/flutter_vector_icons.dart';
+import 'package:multi_vendor_food/common/reusable_text.dart';
+import 'package:multi_vendor_food/constants/constants.dart';
+
+class Heading extends StatelessWidget {
+  const Heading ({super.key, required this.text, this.onTop});
+
+  final String text;
+  final void Function()? onTop;
+
+  @override
+  Widget build(BuildContext context) {
+    return  Container(
+      padding: EdgeInsets.symmetric(horizontal: 12.w),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Padding(padding: EdgeInsets.only(top:10.h),
+          child: ReusableText(style: TextStyle(fontSize: 12.w, color: kDark, fontWeight: FontWeight.bold), text: text),
+          ),
+
+          GestureDetector(
+            onTap: onTop,
+            child: Icon(
+              AntDesign.appstore1,
+              color: kDark,
+              size: 20.sp ,
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
--- lib/views/home/home_page.dart
+++ lib/views/home/home_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:multi_vendor_food/common/custom_appbar.dart';
 import 'package:multi_vendor_food/common/custom_container.dart';
+import 'package:multi_vendor_food/common/heading.dart';
 import 'package:multi_vendor_food/constants/constants.dart';
 import 'package:multi_vendor_food/views/home/widget/category_list.dart';
 
@@ -12,14 +13,19 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: kPrimary,
-        appBar: PreferredSize(preferredSize: Size.fromHeight(130.h),  child: CustomAppBar(), ),
+        appBar: PreferredSize(
+          preferredSize: Size.fromHeight(130.h),
+          child: CustomAppBar(),
+        ),
         body: SafeArea(
             child: CustomContainer(
-                containerContent: const Column(
+                containerContent: Column(
           children: [
-            CategoryList(),
-            // Heading
-            ],
+            const CategoryList(),
+            Heading(text: "Nearby Restaurant", onTop: () {}),
+            Heading(text: "Try Something New ", onTop: () {}),
+            Heading(text: "Food Closer to you", onTop: () {}),
+          ],
         ))));
   }
 }
